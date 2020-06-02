d6f504b37c832c950cb7ea3af884429bebce9dd6
Sebastian Omar de la Vega <61508227+SebaGxT@users.noreply.github.com>
2020-06-02 00:32:49 -0300

    suma de filas en matriz

---
+++ Matriz 7x7 Suma de las filas cargadas con enteros positivos version 1.cpp
@@ -0,0 +1,100 @@
+//Cargar en un vector V de dimensión 7 la suma de las filas de una matriz de 7x7, previamente cargada con enteros positivos. 
+//Librerias
+#include <stdio.h>
+#include <stdlib.h>
+
+//Main
+int main(void){	
+
+//Variables	
+
+	const int FIL=7,COL=7;
+	int f,c,sum,vec[FIL],mat[FIL][COL];
+	
+//Ingreso de datos	
+	
+	printf("Carga de matriz con enteros positivos.\n\n");
+	printf("Se mostrara la suma de cada fila.\n");
+	
+	for(f=0;f<FIL;f++){	
+	
+		for(c=0;c<COL;c++){	
+		
+			do{		
+			
+				printf("\nIngrese un valor fila %d columna %d: ",f+1,c+1);
+				scanf("%d",&mat[f][c]);
+				fflush(stdin);	
+					
+				if(mat[f][c]<0){
+								
+					printf("\n\nIngrese un valor positivo por favor.\n\n");					
+				}	
+							
+			}while(mat[f][c]<0);
+						
+		}	
+			
+	}	
+	
+//Proceso
+	
+	for(f=0;f<FIL;f++){	
+	
+		for(c=0;c<COL;c++){	
+		
+			//Suma de fila
+			sum+=mat[f][c];	
+			
+		}		
+		
+		//Asignación de suma en vector
+		vec[f]=sum;		
+		
+		//Reinicio de suma
+		sum=0;		
+		
+	}	
+//Salida de datos
+
+	printf("\n");	
+	
+	//Número de columna
+	for(c=0;c<COL;c++){	
+		
+		printf("\t  Columna %d",c+1);
+				
+	}	
+	for(f=0;f<FIL;f++){	
+		
+		printf("\nFila %d:\t",f+1);	
+			
+		for(c=0;c<COL;c++){	
+				
+			printf("%8d\t",mat[f][c]);	
+				
+		}		
+		
+		printf("\n");
+				
+	}	
+	
+	//Mensaje de salida
+	
+	printf("\n\nLa suma de cada fila es: \n\n");
+		
+	//Resultado de suma de cada fila
+	for(f=0;f<FIL;f++){	
+		
+		printf("Fila %d es: %d\n",f+1,vec[f]);	
+			
+	}	
+	
+//Pausa y fin de programa	
+
+	printf("\n\n\n\n");
+	system("pause");
+	return 0;	
+		
+}
+
