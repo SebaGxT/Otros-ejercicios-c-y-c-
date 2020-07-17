0ad2458b12b397c2532b544aed80b050b2179bf9
Sebastian Omar de la Vega <61508227+SebaGxT@users.noreply.github.com>
2020-07-16 22:38:07 -0300

    Ejemplo vector como parametro
    Cree un ejemplo para cargar un vector pasándolo como parámetro a una función, uno para número entero y otro para número decimal.

---
+++ Ejemplo vector como parametro en funcion.cpp
@@ -0,0 +1,77 @@
+//Librerias
+#include <stdio.h>
+#include <stdlib.h>
+
+//Funcion de carga
+float cargarvecdecimal(float vecdec[],int tamaniovec){
+	
+	//Recorrido de vector
+	int i;
+	
+	for(i=0;i<tamaniovec;i++){
+	
+		printf("\nIngrese el valor decimal %d: ",i+1);
+		scanf("%f",&vecdec[i]);
+		fflush(stdin);	
+		
+	}
+		
+}
+
+int cargarvecentero(int vecen[],int tamaniovec){
+	
+	//Recorrido de vector
+	int i;
+	
+	for(i=0;i<tamaniovec;i++){
+	
+		printf("\nIngrese el valor entero %d: ",i+1);
+		scanf("%d",&vecen[i]);
+		fflush(stdin);	
+		
+	}
+		
+}
+
+//Main
+int main(){
+
+//Variables	
+	const int TAMANIO=5;
+	int i,vecentero[TAMANIO];
+	float vecdecimal[TAMANIO];
+
+//Poner en cero los vectores	
+	for(i=0;i<TAMANIO;i++){
+		
+		vecentero[i]=0;
+		vecdecimal[i]=0.0;
+		
+	}
+	
+//Llamada a funcion
+	
+	cargarvecentero(vecentero,TAMANIO);
+	cargarvecdecimal(vecdecimal,TAMANIO);
+
+//Impresion por pantalla para verificación	
+	
+	for(i=0;i<TAMANIO;i++){
+		
+		printf("\n%d",vecentero[i]);
+		
+	}
+	
+	for(i=0;i<TAMANIO;i++){
+		
+		printf("\n%.2f",vecdecimal[i]);
+		
+	}
+
+//Pausa y fin de programa
+	
+	printf("\n\n\n\n");
+	system("pause");
+	return 0;
+	
+}
